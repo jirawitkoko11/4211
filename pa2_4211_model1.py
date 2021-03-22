ef826bd8721d2244bb4977e032efaa971eebd98c
jirawitkoko11 <71333131+jirawitkoko11@users.noreply.github.com>
2021-03-22 22:32:18 +0700

    Add files via upload
    fix bugs

--- pa2_4211_model1.py
+++ pa2_4211_model1.py
@@ -116,6 +116,17 @@ class Siamese(nn.Module):
 
 
 
+def save_checkpoint(save_path, model, optimizer, val_loss):
+    if save_path==None:
+        return
+    save_path = save_path 
+    state_dict = {'model_state_dict': model.state_dict(),
+                  'optimizer_state_dict': optimizer.state_dict(),
+                  'val_loss': val_loss}
+
+    torch.save(state_dict, save_path)
+
+    print(f'Model saved to ==> {save_path}')
 
 def plotLoss(train,val,step):
   plt.plot(step,train, label = "train loss")
