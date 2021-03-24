700b036c9040d8d2833d05ea945913bee4c16b67
jirawitkoko11 <71333131+jirawitkoko11@users.noreply.github.com>
2021-03-24 16:14:22 +0700

    Add files via upload

--- utilitiesAndClasses.py
+++ utilitiesAndClasses.py
@@ -14,7 +14,7 @@ from torchsummary import summary
 import torch.optim as optim
 from torch.utils.data import random_split
 
-
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 PATH = './pa2'
 class CustomDataset(Dataset):
   def __init__(self,df_path,txt_path,img_path):
@@ -184,3 +184,62 @@ def load_checkpoint(model, optimizer, save_path):
     optimizer.load_state_dict(state_dict['optimizer_state_dict'])
     val_loss = state_dict['val_loss']
     print(f'Model loaded from <== {save_path}')
+
+
+class Siamese(nn.Module):
+  def __init__(self):
+    super(Siamese, self).__init__()
+    #####Convolutional layers###############
+    self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size= 3,stride=1, padding=1)
+    self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size= 3,stride=1, padding=1)
+    self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size= 3,stride=1, padding=1)
+    self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size= 3,stride=1, padding=1)
+    self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size= 3,stride=1, padding=1)
+    self.conv6 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size= 3,stride=1, padding=1)
+
+    self.bn1 = nn.BatchNorm1d(32*32*32)
+    self.bn2 = nn.BatchNorm1d(32*32*32)
+    self.bn3 = nn.BatchNorm1d(16*16*64)
+    self.bn4 = nn.BatchNorm1d(16*16*128)
+    self.bn5 = nn.BatchNorm1d(16*16*256)
+    self.bn6 = nn.BatchNorm1d(16*16*512)
+
+    self.maxPool = nn.MaxPool2d(kernel_size=2,stride=2,padding=0)
+    self.avgPool = nn.AvgPool2d(kernel_size=16,padding=0)
+    ##########end of Convolutional layers###########
+    
+    ##########linear layers###################
+    self.fc1 = nn.Linear(512,512)
+    self.drop = nn.Dropout(0.5)
+    self.fc2 = nn.Linear(512,1)
+
+    self.sigmoid = nn.Sigmoid()
+
+    ############end of linear layers################
+  def forward(self,pic1,pic2):
+    x = self.aggregation(pic1,pic2)
+    x = F.relu(x)
+    x = self.drop(x)
+    x = self.sigmoid(self.fc2(x))
+    return x.view(-1)
+
+  def getfeature(self,pic):
+    batch_in = pic.shape[0]
+    x = F.relu(self.bn1(self.conv1(pic).view(batch_in,32*32*32)).view(batch_in,32,32,32))
+    x = F.relu(self.bn2(self.conv2(x).view(batch_in,32*32*32)).view(batch_in,32,32,32))
+    x = self.maxPool(x)
+    x = F.relu(self.bn3(self.conv3(x).view(batch_in,64*16*16)).view(batch_in,64,16,16))
+    x = F.relu(self.bn4(self.conv4(x).view(batch_in,128*16*16)).view(batch_in,128,16,16))
+    x = F.relu(self.bn5(self.conv5(x).view(batch_in,256*16*16)).view(batch_in,256,16,16))
+    x = F.relu(self.bn6(self.conv6(x).view(batch_in,512*16*16)).view(batch_in,512,16,16))
+    x = self.avgPool(x)
+    x = x.view(-1,1*512)
+    return x
+
+    #############Aggregation##################
+  def aggregation(self,pic1,pic2):
+    pic1_vector = self.getfeature(pic1)
+    pic2_vector = self.getfeature(pic2)
+    diff = torch.abs(torch.sub(pic1_vector,pic2_vector))
+    return diff
+    #############end of Aggregation##################
