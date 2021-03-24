79c8405f995a1a8608600cb2adb39103ada6a065
jirawitkoko11 <71333131+jirawitkoko11@users.noreply.github.com>
2021-03-24 11:38:33 +0700

    Add files via upload

--- threshold.py
+++ threshold.py
@@ -54,9 +54,9 @@ valid_loader = torch.utils.data.DataLoader(valid, batch_size=128, shuffle=False,
 
 
 
-class Siamese(nn.Module):
+class Siamese3(nn.Module):
   def __init__(self):
-    super(Siamese, self).__init__()
+    super(Siamese3, self).__init__()
     #####Convolutional layers###############
     self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size= 3,stride=1, padding=1)
     self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size= 3,stride=1, padding=1)
@@ -64,6 +64,7 @@ class Siamese(nn.Module):
     self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size= 3,stride=1, padding=1)
     self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size= 3,stride=1, padding=1)
     self.conv6 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size= 3,stride=1, padding=1)
+    self.conv7 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size= 3,stride=1, padding=1) #new
 
     self.bn1 = nn.BatchNorm1d(32*32*32)
     self.bn2 = nn.BatchNorm1d(32*32*32)
@@ -71,6 +72,7 @@ class Siamese(nn.Module):
     self.bn4 = nn.BatchNorm1d(16*16*128)
     self.bn5 = nn.BatchNorm1d(16*16*256)
     self.bn6 = nn.BatchNorm1d(16*16*512)
+    self.bn7 = nn.BatchNorm1d(16*16*512) #new
 
     self.maxPool = nn.MaxPool2d(kernel_size=2,stride=2,padding=0)
     self.avgPool = nn.AvgPool2d(kernel_size=16,padding=0)
@@ -100,12 +102,14 @@ class Siamese(nn.Module):
     x = F.relu(self.bn4(self.conv4(x).view(batch_in,128*16*16)).view(batch_in,128,16,16))
     x = F.relu(self.bn5(self.conv5(x).view(batch_in,256*16*16)).view(batch_in,256,16,16))
     x = F.relu(self.bn6(self.conv6(x).view(batch_in,512*16*16)).view(batch_in,512,16,16))
+    x = F.relu(self.bn6(self.conv7(x).view(batch_in,512*16*16)).view(batch_in,512,16,16)) ##new
     x = self.avgPool(x)
     x = x.view(-1,1*512)
     return x
 
     #############Aggregation##################
   def aggregation(self,pic1,pic2):
+    #print(pic1.shape)
     pic1_vector = self.getfeature(pic1)
     pic2_vector = self.getfeature(pic2)
     diff = torch.abs(torch.sub(pic1_vector,pic2_vector))
