edf69ed2cc92eba9e0e7793ab89894074dc40546
jirawitkoko11 <71333131+jirawitkoko11@users.noreply.github.com>
2021-03-27 03:20:11 +0700

    Add files via upload

--- threshold.py
+++ threshold.py
@@ -1,122 +1,4 @@
-import torch
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
-from PIL import Image
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchsummary import summary
-import torch.optim as optim
-from torch.utils.data import random_split
-
-
-PATH = './pa2'
-class CustomDataset(Dataset):
-  def __init__(self,df_path,txt_path,img_path):
-    df = pd.read_csv(df_path)
-    txt = pd.read_csv(txt_path, header = None)
-    txt.columns = ['id','path']
-    self.df = df
-    self.txt = txt
-    self.imgPath = img_path
-
-  def __getitem__(self,idx):
-    ind1 = self.df.iloc[idx].id1 
-    ind2 = self.df.iloc[idx].id2
-    label = self.df.iloc[idx].target
-    img1_path = os.path.join(PATH,self.txt.iloc[ind1-1].path)
-    img2_path = os.path.join(PATH,self.txt.iloc[ind2-1].path)
-    
-    return self.transformPic(img1_path),self.transformPic(img2_path),label
-    
-  def __len__(self):
-    return len(self.df)
-
-  def transformPic(self,pic_path):
-    Resize = transforms.Compose([transforms.Resize([32,32]),
-                                 transforms.ToTensor(),
-                                 transforms.Normalize(0.5,0.5)])
-    img = Image.open(pic_path).convert('L')
-    return Resize(img)
-
-
-
-train = CustomDataset(os.path.join(PATH,'train.csv'),os.path.join(PATH,'index.txt'),'asd')
-valid = CustomDataset(os.path.join(PATH,'train.csv'),os.path.join(PATH,'index.txt'),'asd')
-test = CustomDataset(os.path.join(PATH,'train.csv'),os.path.join(PATH,'index.txt'),'asd')
-loader = torch.utils.data.DataLoader(train, batch_size=128, shuffle=True, num_workers=2)
-valid_loader = torch.utils.data.DataLoader(valid, batch_size=128, shuffle=False, num_workers=2)
-
-
-
-class Siamese3(nn.Module):
-  def __init__(self):
-    super(Siamese3, self).__init__()
-    #####Convolutional layers###############
-    self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size= 3,stride=1, padding=1)
-    self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size= 3,stride=1, padding=1)
-    self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size= 3,stride=1, padding=1)
-    self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size= 3,stride=1, padding=1)
-    self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size= 3,stride=1, padding=1)
-    self.conv6 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size= 3,stride=1, padding=1)
-    self.conv7 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size= 3,stride=1, padding=1) #new
-
-    self.bn1 = nn.BatchNorm1d(32*32*32)
-    self.bn2 = nn.BatchNorm1d(32*32*32)
-    self.bn3 = nn.BatchNorm1d(16*16*64)
-    self.bn4 = nn.BatchNorm1d(16*16*128)
-    self.bn5 = nn.BatchNorm1d(16*16*256)
-    self.bn6 = nn.BatchNorm1d(16*16*512)
-    self.bn7 = nn.BatchNorm1d(16*16*512) #new
-
-    self.maxPool = nn.MaxPool2d(kernel_size=2,stride=2,padding=0)
-    self.avgPool = nn.AvgPool2d(kernel_size=16,padding=0)
-    ##########end of Convolutional layers###########
-    
-    ##########linear layers###################
-    self.fc1 = nn.Linear(512,512)
-    self.drop = nn.Dropout(0.5)
-    self.fc2 = nn.Linear(512,1)
-
-    self.sigmoid = nn.Sigmoid()
-
-    ############end of linear layers################
-  def forward(self,pic1,pic2):
-    x = self.aggregation(pic1,pic2)
-    x = F.relu(x)
-    x = self.drop(x)
-    x = self.sigmoid(self.fc2(x))
-    return x.view(-1)
-
-  def getfeature(self,pic):
-    batch_in = pic.shape[0]
-    x = F.relu(self.bn1(self.conv1(pic).view(batch_in,32*32*32)).view(batch_in,32,32,32))
-    x = F.relu(self.bn2(self.conv2(x).view(batch_in,32*32*32)).view(batch_in,32,32,32))
-    x = self.maxPool(x)
-    x = F.relu(self.bn3(self.conv3(x).view(batch_in,64*16*16)).view(batch_in,64,16,16))
-    x = F.relu(self.bn4(self.conv4(x).view(batch_in,128*16*16)).view(batch_in,128,16,16))
-    x = F.relu(self.bn5(self.conv5(x).view(batch_in,256*16*16)).view(batch_in,256,16,16))
-    x = F.relu(self.bn6(self.conv6(x).view(batch_in,512*16*16)).view(batch_in,512,16,16))
-    x = F.relu(self.bn6(self.conv7(x).view(batch_in,512*16*16)).view(batch_in,512,16,16)) ##new
-    x = self.avgPool(x)
-    x = x.view(-1,1*512)
-    return x
-
-    #############Aggregation##################
-  def aggregation(self,pic1,pic2):
-    #print(pic1.shape)
-    pic1_vector = self.getfeature(pic1)
-    pic2_vector = self.getfeature(pic2)
-    diff = torch.abs(torch.sub(pic1_vector,pic2_vector))
-    return diff
-    #############end of Aggregation##################
-
-
+from utilitiesAndClasses import *
 
 def pltThreshold(thres,acc,bestT,bestA,save_path):
   plt.plot(thres,acc)
@@ -124,9 +6,10 @@ def pltThreshold(thres,acc,bestT,bestA,save_path):
   plt.ylabel('accuracy')
   plt.title('threshold vs accuracy\n best theta = %.3f which gives %.4f accuracy' % (bestT,bestA))
   plt.savefig(save_path,dpi=300)
+  plt.close()
 
 def getThreshold(valid_loader,model,save_path):
-  theta = np.linspace(0.4,0.9,20, False)
+  theta = np.linspace(0.30,0.6,20, False)
   acc  = []
   bestT = 0
   bestA = 0
@@ -161,10 +44,10 @@ def load_checkpoint(model, optimizer, save_path):
     val_loss = state_dict['val_loss']
     print(f'Model loaded from <== {save_path}')
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model = Siamese3()
-optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay = 0.01)  ######### add only weight_decay
 
 print("findding the best threshold for the model")
-load_checkpoint(model,optimizer,'model6_net.pt')
-getThreshold(valid_loader,model.to(device),'T6.png')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = Siamese()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+load_checkpoint(model,optimizer,'model1_net.pt')
+getThreshold(valid_loader,model.to(device),'T1.png')
