7baa92365b81fe2cf8950d296762a20662e483f7
jirawitkoko11 <71333131+jirawitkoko11@users.noreply.github.com>
2021-03-27 01:03:15 +0700

    Add files via upload

--- optimize3.py
+++ optimize3.py
@@ -5,12 +5,12 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
 model = Siamese()
 model = model.to(device)
-num_epochs = 20
+num_epochs = 30
 eval_every = 10
 total_step = len(loader)*num_epochs
 best_val_loss = None
 criterion = nn.BCELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.1 , weight_decay = 0.00001) #change learning rate to 0.01, add weight_decay
+optimizer = optim.Adam(model.parameters(), lr=0.001 , weight_decay = 0.001) #change learning rate to 0.01, add weight_decay
 
 TRAIN(model, loader, valid_loader, num_epochs, eval_every,
       total_step, criterion, optimizer, best_val_loss, device ,'model4_net.pt','Loss_M4.png')
